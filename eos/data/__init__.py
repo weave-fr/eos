8d00ad3da9cb4e2855aa1b35ff962a3e85304a29
weave-fr <weave@netcourrier.com>
2026-09-21 21:50:31 +0200

    Update __init__.py

--- eos/data/__init__.py
+++ eos/data/__init__.py
@@ -1,7 +1,10 @@
 """EOS data pipeline components."""
 
 from .dataset import EOSTextDataset
+from .tokenize import tokenize_file, tokenize_text
 
 __all__ = [
     "EOSTextDataset",
+    "tokenize_file",
+    "tokenize_text",
 ]
